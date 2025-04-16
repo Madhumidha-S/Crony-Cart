c2b08510874b77090dad843f35971d322078294c
M. Vinay <license@merawla.in>
2025-04-16 18:40:01 +0530

    Added product routes

--- src/routes/product_routes.js
+++ src/routes/product_routes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import {createProduct, getAllProducts, getProductById, updateProduct, deleteProduct} from "../controller/productController.js";
 
 const router = express.Router();
 router.post("/product", createProduct);
 router.get("/product", getAllProducts);
 router.get("/product/:id", getProductById);
 router.put("/product/:id", updateProduct);
-router.delete("/product", deleteProductById);
+router.delete("/product", deleteProduct);
