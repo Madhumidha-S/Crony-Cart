299bbb9b205eecd93d8aee7e2b3e7c9c5d616907
M. Vinay <license@merawla.in>
2025-04-16 16:56:02 +0530

    Created Product routes

---
+++ src/routes/product_routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+router.post("/product", createProduct);
+router.get("/product", getAllProducts);
+router.get("/product/:id", getProductById);
+router.put("/product/:id", updateProduct);
+router.delete("/product", deleteProduct);
